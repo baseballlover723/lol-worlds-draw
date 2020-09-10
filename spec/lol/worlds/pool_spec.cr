3c1c151d61c0b274bee7131698277e2c0068cab0
Philip Ross <dev@baseballlover723.com>
2020-09-10 13:24:30 -0700

    made spec for pools more modular

--- spec/lol/worlds/pool_spec.cr
+++ spec/lol/worlds/pool_spec.cr
@@ -3,48 +3,30 @@ require "../.././spec_helper"
 module Lol::Worlds
   describe Pool do
     describe "json" do
+      enums = {"1": Pool::One, "2": Pool::Two, "3": Pool::Three, "4": Pool::Four}
+
       describe "serializes" do
-        it "one correctly" do
-          Pool::One.to_json.should eq("1")
-        end
-
-        it "two correctly" do
-          Pool::Two.to_json.should eq("2")
-        end
-
-        it "three correctly" do
-          Pool::Three.to_json.should eq("3")
-        end
-
-        it "four correctly" do
-          Pool::Four.to_json.should eq("4")
+        enums.each do |json_value, object|
+          it "#{object} correctly" do
+            object.to_json.should eq(json_value.to_s)
+          end
         end
       end
 
       describe "deserializes" do
-        it "one correctly" do
-          Pool.from_json("1").should eq(Pool::One)
+        enums.each do |json_value, object|
+          it "#{object} correctly" do
+            Pool.from_json(json_value.to_s).should eq(object)
+          end
         end
 
-        it "two correctly" do
-          Pool.from_json("2").should eq(Pool::Two)
-        end
-
-        it "three correctly" do
-          Pool.from_json("3").should eq(Pool::Three)
-        end
-
-        it "four correctly" do
-          Pool.from_json("4").should eq(Pool::Four)
-        end
-
-        [0, 5].each do |numb|
+        {0, 5}.each do |numb|
           it "throws an argument error for #{numb}" do
             expect_raises(Exception) do
               Pool.from_json(numb.to_s)
             end
           end
-        end  
+        end
       end
     end
   end
