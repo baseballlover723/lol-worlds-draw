89ac744906141dba4094f98f57d13a913744e151
Philip Ross <dev@baseballlover723.com>
2020-09-10 13:04:32 -0700

    converted pool to serialize using json

--- spec/lol/worlds/pool_spec.cr
+++ spec/lol/worlds/pool_spec.cr
@@ -3,27 +3,49 @@ require "lol/worlds/pool"
 
 module Lol::Worlds
   describe Pool do
-    it "gives one for 1" do
-      Pool.get_pool(1).should eq(Pool::One)
-    end
-
-    it "gives two for 2" do
-      Pool.get_pool(2).should eq(Pool::Two)
-    end
-
-    it "gives three for 3" do
-      Pool.get_pool(3).should eq(Pool::Three)
-    end
-
-    it "gives four for 4" do
-      Pool.get_pool(4).should eq(Pool::Four)
-    end
-
-    [0, 5].each do |numb|
-      it "throws an argument error if for #{numb}" do
-        expect_raises(ArgumentError) do
-          Pool.get_pool(numb)
+    describe "json" do
+      describe "serializes" do
+        it "one correctly" do
+          Pool::One.to_json.should eq("1")
         end
+
+        it "two correctly" do
+          Pool::Two.to_json.should eq("2")
+        end
+
+        it "three correctly" do
+          Pool::Three.to_json.should eq("3")
+        end
+
+        it "four correctly" do
+          Pool::Four.to_json.should eq("4")
+        end
+      end
+
+      describe "deserializes" do
+        it "one correctly" do
+          Pool.from_json("1").should eq(Pool::One)
+        end
+
+        it "two correctly" do
+          Pool.from_json("2").should eq(Pool::Two)
+        end
+
+        it "three correctly" do
+          Pool.from_json("3").should eq(Pool::Three)
+        end
+
+        it "four correctly" do
+          Pool.from_json("4").should eq(Pool::Four)
+        end
+
+        [0, 5].each do |numb|
+          it "throws an argument error for #{numb}" do
+            expect_raises(Exception) do
+              Pool.from_json(numb.to_s)
+            end
+          end
+        end  
       end
     end
   end
