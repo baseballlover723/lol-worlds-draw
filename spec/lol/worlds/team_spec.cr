2963eb444de87b102cd2e182515142126aa29130
Philip Ross <dev@baseballlover723.com>
2020-09-10 21:33:33 -0700

    added tests for team

--- spec/lol/worlds/team_spec.cr
+++ spec/lol/worlds/team_spec.cr
@@ -23,5 +23,33 @@ module Lol::Worlds
         team.pool.should eq(Pool::One)
       end
     end
+
+    it "reserializes to itself" do
+      team = Team.new("TSM", Pool::One, Region::North_America)
+      Team.from_json(team.to_json).should eq(team)
+    end
+
+    describe "serializes" do
+      it "a team correctly" do
+        team = Team.new("TSM", Pool::One, Region::North_America)
+        team.to_json.should eq("{\"name\":\"TSM\",\"pool\":1,\"region\":\"North_America\"}")
+      end
+    end
+
+    describe "deserializes" do
+      it "can read all of the seeds" do
+        seeds = File.read("files/seeds.json")
+        teams = Array(Team).from_json(seeds)
+      end
+
+      it "can read from a json string" do
+        json = {name: "TSM", pool: 1, region: "North_America"}.to_json
+        team = Team.from_json(json)
+
+        team.name.should eq("TSM")
+        team.pool.should eq(Pool::One)
+        team.region.should eq(Region::North_America)
+      end
+    end
   end
 end
--- src/lol/worlds/team.cr
+++ src/lol/worlds/team.cr
@@ -16,5 +16,9 @@ module Lol::Worlds
     def initialize(@name : String, pool : Int, @region : Region)
       @pool = Pool.from_json(pool.to_s)
     end
+
+    def ==(o : self) : Bool
+      name == o.name && pool == o.pool && region == o.region
+    end
   end
 end
