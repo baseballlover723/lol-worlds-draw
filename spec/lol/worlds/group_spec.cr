4369ffe606f31733e7922988a22d5cce14535ef7
Philip Ross <dev@baseballlover723.com>
2020-09-12 17:04:36 -0700

    added equals test to group

--- spec/lol/worlds/group_spec.cr
+++ spec/lol/worlds/group_spec.cr
@@ -5,5 +5,23 @@ module Lol::Worlds
     it "is a set4 of Teams" do
       Group.new.should be_a(Set4(Team))
     end
+
+    describe "==" do
+      it "is equal if they have the same teams" do
+        team1 = Team.new("TSM", Pool::One, Region::North_America)
+        team2 = Team.new("Rouge", Pool::Three, Region::Europe)
+        group1 = Group{team1, team2}
+        group2 = Group{team1, team2}
+        group1.should eq(group2)
+      end
+
+      it "is equal if they have the same teams regardless of order" do
+        team1 = Team.new("TSM", Pool::One, Region::North_America)
+        team2 = Team.new("Rouge", Pool::Three, Region::Europe)
+        group1 = Group{team1, team2}
+        group2 = Group{team2, team1}
+        group1.should eq(group2)
+      end
+    end
   end
 end
